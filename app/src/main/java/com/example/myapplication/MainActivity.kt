eecda5ede5884f953646711914251646ccaa7e63
devrangga <devranggahazzamahiswara@mail.ugm.ac.id>
2023-09-26 18:50:17 +0700

    PPAPB 6

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,56 +1,40 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Spinner
+import android.widget.TimePicker
 import android.widget.Toast
-import com.example.myapplication.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var provinces: Array<String>
-    private val countries = arrayOf(
-        "Indonesia",
-        "United States",
-        "United Kingdom",
-        "Germany",
-        "France",
-        "Australia",
-        "Japan",
-        "China",
-        "Brazil",
-        "Canada"
-    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        provinces = resources.getStringArray(R.array.provinces)
-        with(binding){
-            val adapterCountry = ArrayAdapter(this@MainActivity,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, countries)
-            adapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerCountry.adapter = adapterCountry
-            val adapterProvinces = ArrayAdapter(this@MainActivity,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, provinces)
-            adapterProvinces.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinnerProvinces.adapter = adapterProvinces
+        setContentView(R.layout.activity_main)
 
-            datePicker.init(
-                datePicker.year,
-                datePicker.month,
-                datePicker.dayOfMonth
-            ) { _, year, monthOfYear, dayOfMonth ->
-                val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
-                Toast.makeText(this@MainActivity, selectedDate, Toast.LENGTH_SHORT).show()
-// Gunakan selectedDate sesuai kebutuhan Anda
-            }
+        val timePicker = findViewById<TimePicker>(R.id.timePicker)
+        val datePicker = findViewById<DatePicker>(R.id.datePicker)
+        val spinnerTicketType = findViewById<Spinner>(R.id.spinnerTicketType)
+        val buttonPesan = findViewById<Button>(R.id.buttonPesan)
 
-            timePicker.setOnTimeChangedListener { view, hourOfDay, minute ->
-                val selectedTime = String.format("%02d:%02d", hourOfDay, minute)
-                Toast.makeText(this@MainActivity, selectedTime, Toast.LENGTH_SHORT).show()
-// Gunakan selectedTime sesuai kebutuhan Anda
-            }
+        // Menambahkan opsi tiket ke dalam Spinner
+        val ticketTypes = arrayOf("Bisnis", "Ekonomi", "Dll") // Ganti dengan daftar jenis tiket yang Anda inginkan
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ticketTypes)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerTicketType.adapter = adapter
+
+        buttonPesan.setOnClickListener {
+            val selectedTime = "${timePicker.hour}:${timePicker.minute}"
+            val selectedDate = "${datePicker.dayOfMonth}/${datePicker.month + 1}/${datePicker.year}"
+            val selectedTicketType = spinnerTicketType.selectedItem.toString()
+
+            val toastMessage = "Anda telah memesan tiket pada tanggal $selectedDate, jam $selectedTime, dengan jenis tiket $selectedTicketType."
+            Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show()
         }
     }
 }
